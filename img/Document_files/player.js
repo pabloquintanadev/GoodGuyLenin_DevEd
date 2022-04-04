686b797b44575b88902fa8a553e5cf7f0a5a60be
wablopilson <wablopilson@gmail.com>
2022-04-04 21:51:33 +0200

    bullets y limpieza

---
+++ img/Document_files/player.js
@@ -0,0 +1,36 @@
+class Player {
+    constructor(ctx, posX, posY, width, heigth) {
+        this.ctx = ctx
+        this.playerPos = { x: posX, y: posY }
+        this.playerSize = { w: width, h: heigth }
+
+        this.imageInstance = undefined
+
+        this.init()
+    }
+
+    init() {
+        this.imageInstance = new Image()
+        this.imageInstance.src = './img/player.png'
+    }
+
+    draw() {
+        this.ctx.drawImage(this.imageInstance, this.playerPos.x, this.playerPos.y, this.playerSize.w, this.playerSize.h)
+    }
+
+    moveLeft() {
+        this.playerPos.x -= 50
+    }
+
+    moveRight() {
+        this.playerPos.x += 50
+    }
+
+    moveUp() {
+        this.playerPos.y -= 50
+    }
+
+    moveDown() {
+        this.playerPos.y += 50
+    }
+}
